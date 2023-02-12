dc2a16b73141ba285cfa60d325883b5d0f88129c
pavel <paveljinarpopei@gmail.com>
2023-02-12 11:23:40 +0200

    Am construit butoanele, am o functie care genereaza un numar random pentru a determina butonul castigator, mai trebuie sa dau un id specific fiecarui buton pentru a putea afisa daca este sau nu castigator

--- button.js
+++ button.js
@@ -1,20 +1,29 @@
+let optionNumber = 0;
+let winnerButton = 0;
+
+function random (min, max) {
+    winnerButton = Math.floor(Math.random() * (max - min) + min);
+    return winnerButton;
+}
 function isWinner () {
-    document.getElementById("choose").innerText =
-        "isWinner";
+    let y = document.getElementById("number").value;
+    console.log(random(0, y));
+    document.getElementById("win").innerHTML =
+        "you win";
 }
 function GenerateButton () {
-    let x = 0;
+    let x;
     x = document.getElementById('number').value;
     for (let i = 0; i < x; ++i) {
         document.getElementById("createButton").innerHTML +=
             '<br>' +
             '<div class="card-body" >\n' +
-            '            <button type="button" onclick="isWinner()" \n' +
+            '            <button type="button" \n onclick="isWinner()"' +
             '                class="btn btn-success">\n' +
-            '            Choose\n' +
-            '<h1 id = "choose" >' +
-            '</h1>' +
+            '            <p id = "win"> Choose\n'+ i
+            '</p>'
             '</button>\n' +
             '</div>'
+        ++optionNumber;
     }
 }
